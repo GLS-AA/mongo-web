3dec3dedd72230725745cd88bbfdafb4b2ab7677
sommai.k <sommai.k@gmail.com>
2021-12-20 14:18:18 +0700

    add filter button

--- src/app/pages/rt-common/rt-common.component.ts
+++ src/app/pages/rt-common/rt-common.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Apollo, gql } from 'apollo-angular';
+import { Apollo, gql, QueryRef } from 'apollo-angular';
 
 @Component({
   selector: 'app-rt-common',
@@ -9,52 +9,62 @@ import { Apollo, gql } from 'apollo-angular';
 export class RtCommonComponent implements OnInit {
   rtcommon89: any[] = [];
   visitid: string = 'I48-21-000034';
+  saleQuery: QueryRef<any>;
 
-  constructor(private apollo: Apollo) {}
-
-  ngOnInit(): void {
-    this.apollo
-      .watchQuery({
-        variables: {
-          filter: {
-            orguid: '5b20f438d1202e29ce16f710',
-            visitid: this.visitid,
-          },
+  constructor(private apollo: Apollo) {
+    this.saleQuery = this.apollo.watchQuery<any>({
+      variables: {
+        filter: {
+          orguid: '5b20f438d1202e29ce16f710',
+          visitid: this.visitid,
         },
-        query: gql`
-          query SALE($filter: ReportFilter!) {
-            rtcommon89(filter: $filter) {
-              visitid
-              countbill
-              sumbill
-              bed1 {
-                _id
-                bed {
-                  code
-                }
+      },
+      query: gql`
+        query SALE($filter: ReportFilter!) {
+          rtcommon89(filter: $filter) {
+            visitid
+            countbill
+            sumbill
+            bed1 {
+              _id
+              bed {
+                code
               }
-              bedlast {
-                _id
-                bed {
-                  code
-                }
+            }
+            bedlast {
+              _id
+              bed {
+                code
               }
-              bedoccupancy {
+            }
+            bedoccupancy {
+              _id
+              beduid
+              bed {
                 _id
-                beduid
-                bed {
-                  _id
-                  code
-                  name
-                  description
-                }
+                code
+                name
+                description
               }
             }
           }
-        `,
-      })
-      .valueChanges.subscribe((resp: any) => {
-        this.rtcommon89 = resp?.data?.rtcommon89;
-      });
+        }
+      `,
+    });
+  }
+
+  ngOnInit(): void {
+    this.saleQuery.valueChanges.subscribe((resp: any) => {
+      this.rtcommon89 = resp?.data?.rtcommon89;
+    });
+  }
+
+  filterClick() {
+    this.saleQuery.refetch({
+      filter: {
+        orguid: '5b20f438d1202e29ce16f710',
+        visitid: this.visitid,
+      },
+    });
   }
 }
